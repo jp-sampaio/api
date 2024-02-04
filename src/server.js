4b01f437a13d0a71ab1e1161ea4b34c6bf076848
jpsampaio <jpsampaiosampaio@gmail.com>
2024-02-04 10:31:45 -0300

    add file server,js

--- src/server.js
+++ src/server.js
@@ -5,8 +5,22 @@ const express = require("express");
 const app = express();
 
 // Utilizando os verbs https, nesse exemplo o get ( leitura );
-app.get("/message", (request, response) => {
-  response.send("Hello World!");
+// Route Params
+app.get("/message/:id/:user", (request, response) => {
+  // Desistruturando o request.params
+  const { id, user } = request.params;
+
+  response.send(`
+    Mensagem ID: ${id}.
+    Para o usuário ${user}.
+  `);
+});
+
+// Query Params
+app.get("/users", (request, response) => {
+  const { page, limit } = request.query;
+
+  response.send(`Página: ${page}. Mostrar: ${limit}.`);
 });
 
 // Nesse ponto eu crio uma porta que é onde vai ser o endereço da requisição
